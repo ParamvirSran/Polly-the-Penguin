7f403e606580dfc6a52104fc05040751559f4509
Nishanth Pirakalathan <npirakal@uwo.ca>
2023-11-08 20:22:05 -0500

    tts

--- TextToSpeech.cpp
+++ TextToSpeech.cpp
@@ -7,7 +7,14 @@
 void TextToSpeech::speak(const std::string& text) {
     std::string command = "espeak \"" + text + "\"";
     int result = std::system(command.c_str());
-    if (result != 0) {
-        throw std::runtime_error("Failed to invoke espeak.");
-    }
 }
+
+/*
+Error chevking version:
+
+int TextToSpeech::speak(const std::string& text) {
+    std::string command = "espeak \"" + text + "\"";
+    int result = std::system(command.c_str());
+    return result == 0 ? 1 : 0; // Return 1 if the command was successful, 0 otherwise
+}
+*/
