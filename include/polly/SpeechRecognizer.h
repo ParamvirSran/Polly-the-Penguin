e1491370094606fba453a09e59775c753d1cb395
Hartej <hrandh9@uwo.ca>
2023-12-01 16:11:48 -0500

    doxygen comments

--- include/polly/SpeechRecognizer.h
+++ include/polly/SpeechRecognizer.h
@@ -1,3 +1,18 @@
+/**
+ * @file       SpeechRecognizer.h
+ * @brief      Declaration of the SpeechRecognizer class for performing speech recognition using Google Cloud Speech API.
+ * 
+ * @note       
+ * @history:
+ *   Version   Date            Author          Modification    Email
+ *   V1.0.0    Nov-28-2023     Saad Mahmood, Nishanth Pirakalathan, Hartej Singh Randhawa, Taimur Sikander, Paramvir Sransmahmo43@uwo.ca, npirakal@uwo.ca, hrandh9@uwo.ca, tsikande@uwo.ca, psran2@uwo.ca
+ * 
+ * @verbatim
+ * ==============================================================================
+ * 
+ * ==============================================================================
+ * @endverbatim
+ */
 #ifndef SPEECHRECOGNIZER_H
 #define SPEECHRECOGNIZER_H
 
@@ -6,24 +21,50 @@
 #include <curl/curl.h>
 #include <nlohmann/json.hpp>
 
+/**
+ * @class SpeechRecognizer
+ * @brief Class for performing speech recognition using Google Cloud Speech API.
+ */
 class SpeechRecognizer {
 public:
-    // Constructor to initialize with an API key
+    /**
+     * @brief Constructor to initialize SpeechRecognizer with an API key.
+     * @param[in] apiKey The API key for the speech service.
+     */
     SpeechRecognizer(const std::string &apiKey);
 
-    // Perform speech recognition on audio buffer
+    /**
+     * @brief Perform speech recognition on an audio buffer.
+     * @param[in] audioBuffer The audio buffer to perform speech recognition on.
+     * @return The transcribed text from the speech recognition.
+     */
     std::string performSpeechRecognition(std::vector<int16_t> audioBuffer);
 
-    // Extract transcribed text from JSON response
+    /**
+     * @brief Extract transcribed text from JSON response.
+     * @param[in] jsonResponse The JSON response from the speech recognition service.
+     * @return The transcribed text.
+     */
     std::string getTranscribedText(const std::string &jsonResponse);
 
 private:
-    std::string apiKey; // API key for the speech service
+    std::string apiKey; ///< API key for the speech service.
 
-    // Callback for writing data received from CURL
+    /**
+     * @brief Callback for writing data received from CURL.
+     * @param[in] contents Pointer to the received data.
+     * @param[in] size Size of each element.
+     * @param[in] nmemb Number of elements.
+     * @param[out] s String to store the received data.
+     * @return The size of the received data.
+     */
     static size_t curlWriteCallback(void *contents, size_t size, size_t nmemb, std::string *s);
 
-    // Encode audio buffer to Base64
+    /**
+     * @brief Encode audio buffer to Base64.
+     * @param[in] buffer The audio buffer to be encoded.
+     * @return The Base64 encoded audio.
+     */
     std::string base64Encode(const std::vector<int16_t> &buffer);
 };
 
