e148652dbb2aa994b5cb5922227e3c280bb4f58f
Paramvir Sran <paramvirsran@gmail.com>
2024-05-08 15:08:41 -0400

    adding build files

---
+++ build/CMakeFiles/Polly.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/parm/Desktop/Programming/C++/Polly/src/Display.cpp" "CMakeFiles/Polly.dir/src/Display.cpp.o" "gcc" "CMakeFiles/Polly.dir/src/Display.cpp.o.d"
+  "/Users/parm/Desktop/Programming/C++/Polly/src/LLMProcessing.cpp" "CMakeFiles/Polly.dir/src/LLMProcessing.cpp.o" "gcc" "CMakeFiles/Polly.dir/src/LLMProcessing.cpp.o.d"
+  "/Users/parm/Desktop/Programming/C++/Polly/src/Polly.cpp" "CMakeFiles/Polly.dir/src/Polly.cpp.o" "gcc" "CMakeFiles/Polly.dir/src/Polly.cpp.o.d"
+  "/Users/parm/Desktop/Programming/C++/Polly/src/TextToSpeech.cpp" "CMakeFiles/Polly.dir/src/TextToSpeech.cpp.o" "gcc" "CMakeFiles/Polly.dir/src/TextToSpeech.cpp.o.d"
+  "/Users/parm/Desktop/Programming/C++/Polly/src/VoiceCustomization.cpp" "CMakeFiles/Polly.dir/src/VoiceCustomization.cpp.o" "gcc" "CMakeFiles/Polly.dir/src/VoiceCustomization.cpp.o.d"
+  "/Users/parm/Desktop/Programming/C++/Polly/src/main.cpp" "CMakeFiles/Polly.dir/src/main.cpp.o" "gcc" "CMakeFiles/Polly.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Polly.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Polly.
+# This may be replaced when dependencies are built.
